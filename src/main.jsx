da918adc089d1db7dfc7a7d9fe4203a902023935
Bhupendra <sainibhupendra50042@gmail.com>
2024-12-25 15:21:30 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // BrowserRouter जोड़ा गया
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/Sudo1-project">
+      {" "}
+      {/* basename सेट किया */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
